7ad37c6a37ef26c5090f77f4baa14567a3b0f561
Fabio Mangione <fabio.mangione@gmail.com>
2017-09-06 00:16:53 +0100

    Changed endpoint

--- json-server/middleware.js
+++ json-server/middleware.js
@@ -1,16 +1,14 @@
 const moment = require('moment');
 
 module.exports = (req, res, next) => {
-
-  if (req.method === 'POST' && '/anniversaries'.indexOf(req.originalUrl) > -1) {
-    req.body.id = new Date().getTime();
-
+  if (req.method === 'POST' && '/employees'.indexOf(req.originalUrl) > -1) {
     const hireDate = moment(req.body.hireDate);
-    console.log('hireDate', hireDate);
     const anniversaryDate = hireDate.add(1, 'year');
-    console.log('hireDate', hireDate);
-    console.log('anniversaryDate', anniversaryDate);
     req.body.anniversaryDate = anniversaryDate.format('YYYY-MM-DD');
+
+    req.originalUrl = req.originalUrl.replace('/employees', '/anniversaries');
+    console.log('Body', req.body);
+    console.log('URL', req.originalUrl);
   }
   next();
 };
